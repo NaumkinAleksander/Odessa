08f9a3b09ec7466da90fd5e7c28fd68fee6bcf82
NaumkinAleksander <naumkin93@gmail.com>
2022-06-23 23:23:23 +0300

    added buttom

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ $('.hamburger-btn').on('click', function (e) {
   $(this).toggleClass('hamburger-btn_active');
   $('.hamburger-nav').toggleClass('hamburger-nav_active');
 });
+
+$('.menu-btn').on('click', function (e) {
+  e.preventDefault();
+  $(this).toggleClass('menu-btn_active');
+  $('.menu-nav').toggleClass('menu-nav_active');
+});
